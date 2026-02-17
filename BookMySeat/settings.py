3f278a5b66281ce4513ba2edc58ef26a068b0690
9117aryashinde-ai <9117aryashinde@gmail.com>
2026-02-17 18:20:10 +0530

    Configure settings usings environment variables

--- BookMySeat/settings.py
+++ BookMySeat/settings.py
@@ -86,13 +86,24 @@ WSGI_APPLICATION = 'BookMySeat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+
+if DATABASE_URL :
+    # For production purpose
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
     }
-}
-DATABASES['default'] = dj_database_url.parse('postgresql://bookmyseat_z6ht_user:8F0NMV6BYmygc0g7N8VCrNpwRQ53PYQL@dpg-d69vjc0gjchc73cnce10-a.virginia-postgres.render.com/bookmyseat_z6ht')
+else:
+    # For local testing
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+# DATABASES['default'] = dj_database_url.parse('postgresql://bookmyseat_db_4rxm_user:klFZdh7hvZZE6I2Sju794tWw2hywrMVv@dpg-d6a4d9hr0fns738d6ea0-a.virginia-postgres.render.com/bookmyseat_db_4rxm')
 # 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
