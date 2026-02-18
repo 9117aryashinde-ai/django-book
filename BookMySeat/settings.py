c410053d0dc460f3c0314409177ca602be4a25bc
9117aryashinde-ai <9117aryashinde@gmail.com>
2026-02-18 17:32:10 +0530

    Fix email configurations

--- BookMySeat/settings.py
+++ BookMySeat/settings.py
@@ -151,7 +151,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = os.environ.get('EMAIL_HOST') # Gmail's email server
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587)) # Secure Port
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS') == True # Encrypt Email
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS') == "True" # Encrypt Email
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') # Sender's Email
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') # App Password
