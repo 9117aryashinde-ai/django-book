4620aeba5c86afd7bb98a1991e3d4fe7769dce50
9117aryashinde-ai <9117aryashinde@gmail.com>
2026-02-18 14:03:52 +0530

    Settings modified

--- BookMySeat/settings.py
+++ BookMySeat/settings.py
@@ -149,9 +149,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # USE SMTP TO SEND EMAILS
 
-EMAIL_HOST = 'smtp.gmail.com' # Gmail's email server
-EMAIL_PORT = 587 # Secure Port
-EMAIL_USE_TLS = True # Encrypt Email
+EMAIL_HOST = os.environ.get('EMAIL_HOST') # Gmail's email server
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587)) # Secure Port
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS') == True # Encrypt Email
 
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') # Sender's Email
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') # App Password
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') # Sender's Email
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') # App Password
